3b0f2237427335171f37e0069281cbdf38363de0
vito <vito@chtrak.com>
2025-02-03 13:36:51 +0800

    更新网站基本信息

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Koibumi Blog';
+export const SITE_TITLE = 'Vito Blog';
 
-export const SITE_DESCRIPTION = 'A blog about SolidJS, Astro, and koibumi design.';
+export const SITE_DESCRIPTION = 'A blog about programming, technology, and other things that interest me.';
 
 export const FAVICON_SRC = '/favicon.svg';
 
 export const AVATAR_SRC = '/avatar.avif';
 
-export const BIO = "This is a demo site for Koibumi Astro Theme. Fork this from github and start your own blog!";
+export const BIO = "This is a demo blog for vito!";
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
 
-export const COPYRIGHT_NAME = 'Koibumi Design';
+export const COPYRIGHT_NAME = 'vito Design';
 
